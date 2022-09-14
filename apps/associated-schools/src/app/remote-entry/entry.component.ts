9c70b125b5a1fbd4355ea08abca625823fa277b8
Aman Jat <aman@sleeksky.com>
2022-09-14 16:45:19 +0530

    removed welcome html

--- apps/associated-schools/src/app/remote-entry/entry.component.ts
+++ apps/associated-schools/src/app/remote-entry/entry.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ng-mf-associated-schools-entry',
-  template: `<ng-mf-nx-welcome></ng-mf-nx-welcome>`,
+  templateUrl: './entry.component.html',
 })
 export class RemoteEntryComponent {}

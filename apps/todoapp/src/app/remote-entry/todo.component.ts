c56e5ffab962b8a95dfd9d5cc86122d68ac735df
Aman Jat <aman@sleeksky.com>
2022-09-20 20:22:00 +0530

    todo css is added

--- apps/todoapp/src/app/remote-entry/todo.component.ts
+++ apps/todoapp/src/app/remote-entry/todo.component.ts
@@ -1,7 +1,35 @@
 import { Component } from '@angular/core';
-
+import * as uuid from 'uuid';
 @Component({
   selector: 'ng-mf-todoapp',
-  template: `<p>Todo App</p>`,
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent {}
+export class TodoComponent {
+  title = 'todo';
+  editableId: string | null = null;
+
+  todos: { id: string; title: string }[] = [
+    { id: 'abc', title: 'Take a Shower' },
+    { id: 'abd', title: 'Finihs your breakfast' },
+  ];
+
+  addTodo = (todo: string) => {
+    this.todos.push({ id: uuid.v4(), title: todo });
+  };
+
+  deleteTodo = (id: string) => {
+    const newArr = this.todos.filter((todo) => todo.id !== id);
+    this.todos = newArr;
+  };
+
+  setEditing = (payload: string | null) => {
+    this.editableId = payload;
+  };
+
+  editTodo = (id: string, value: string) => {
+    const index = this.todos.findIndex((todo) => todo.id == id);
+    this.todos[index].title = value;
+    this.editableId = null;
+  };
+}

1205d821c7a3191fa9d83e74af0728cc4df68c74
Aman Jat <aman@sleeksky.com>
2022-09-20 16:36:57 +0530

    school detail component's css has been removed

--- apps/associated-schools/src/app/remote-entry/school-detail/school.component.ts
+++ apps/associated-schools/src/app/remote-entry/school-detail/school.component.ts
@@ -4,7 +4,6 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'ng-mf-school-detail',
   templateUrl: './school.component.html',
-  styleUrls: ['./school.component.css'],
 })
 export class SchoolDetailComponent {
   constructor(private route: Router) {}

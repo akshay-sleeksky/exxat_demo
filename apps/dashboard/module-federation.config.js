2f55d110a3bf22b4bf7cde859f32b85b30b6b08c
Aman Jat <aman@sleeksky.com>
2022-09-21 18:01:03 +0530

    merge coflicts error

--- apps/dashboard/module-federation.config.js
+++ apps/dashboard/module-federation.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   name: 'dashboard',
-<<<<<<< HEAD
   exposes: {
     './Module': 'apps/dashboard/src/app/remote-entry/entry.module.ts',
   },
-=======
-  remotes: ['associated-schools', 'student-placement', 'student-detail-mui'],
->>>>>>> c51a243 (student_placement_commits)
 };

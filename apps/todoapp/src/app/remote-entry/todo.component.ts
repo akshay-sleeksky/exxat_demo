2a4456911381ab472bbe90e6463276dea7c4fea2
Aman Jat <aman@sleeksky.com>
2022-09-21 12:50:27 +0530

    states are addded

--- apps/todoapp/src/app/remote-entry/todo.component.ts
+++ apps/todoapp/src/app/remote-entry/todo.component.ts
@@ -1,35 +1,43 @@
-import { Component } from '@angular/core';
-import * as uuid from 'uuid';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { addTodo, removeTodo, loadTodos } from '../state/todos/todo.actions';
+import { Todo } from './todo.model';
+import { selectAllTodos } from '../state/todos/todo.selectors';
+
 @Component({
   selector: 'ng-mf-todoapp',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
 })
-export class TodoComponent {
-  title = 'todo';
+export class TodoComponent implements OnInit {
+  // public allTodos$ = this.store.select(selectAllTodos);
+  public todo = '';
   editableId: string | null = null;
 
-  todos: { id: string; title: string }[] = [
-    { id: 'abc', title: 'Take a Shower' },
-    { id: 'abd', title: 'Finihs your breakfast' },
-  ];
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.store.dispatch(loadTodos());
+  }
 
   addTodo = (todo: string) => {
-    this.todos.push({ id: uuid.v4(), title: todo });
+    // this.todos.push({ id: uuid.v4(), content: todo });
+    this.store.dispatch(addTodo({ content: todo }));
   };
 
   deleteTodo = (id: string) => {
-    const newArr = this.todos.filter((todo) => todo.id !== id);
-    this.todos = newArr;
+    // const newArr = this.todos.filter((todo) => todo.id !== id);
+    // this.todos = newArr;
+    this.store.dispatch(removeTodo({ id: id }));
   };
 
   setEditing = (payload: string | null) => {
     this.editableId = payload;
   };
 
-  editTodo = (id: string, value: string) => {
-    const index = this.todos.findIndex((todo) => todo.id == id);
-    this.todos[index].title = value;
-    this.editableId = null;
-  };
+  // editTodo = (id: string, value: string) => {
+  //   const index = this.todos.findIndex((todo) => todo.id == id);
+  //   this.todos[index].content = value;
+  //   this.editableId = null;
+  // };
 }
